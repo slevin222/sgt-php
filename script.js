2defd64085d33d4310d421d9df32fdad8b555b7b
Shawn Levin <shawnlevin22@gmail.com>
2018-03-28 23:18:07 -0700

    cleaned up code order workign on delete student sccuracy

--- script.js
+++ script.js
@@ -22,6 +22,185 @@ function addClickHandlersToElements() {
 
 }
 
+function handleAddClicked() {
+    var name = $("#studentName").val();
+    var course = $("#studentCourse").val();
+    var grade = $("#studentGrade").val();
+    if (typeof name !== 'string' || name.length < 2 || !isNaN(name)) {
+        $(".errorText").text('Name must be specified');
+        $("#errorDisplay").modal('show');
+        return;
+    }
+    if (typeof course !== 'string' || course.length < 2 || !isNaN(course)) {
+        $(".errorText").text('Course must be specified');
+        $("#errorDisplay").modal('show');
+        return;
+    }
+    if (isNaN(grade) || grade < 0 || grade > 100 || grade.length < 1) {
+        $(".errorText").text('Grade must be a number between 0 - 100');
+        $("#errorDisplay").modal('show');
+        return;
+    }
+
+    addStudent(name, course, grade);
+}
+
+function handleCancelClick() {
+    $("#studentName").val("");
+    $("#studentCourse").val("");
+    $("#studentGrade").val("");
+}
+
+function clearAddStudentFormInputs() {
+    $("#studentName").val("");
+    $("#studentCourse").val("");
+    $("#studentGrade").val("");
+}
+
+function showLoadModal() {
+    var today = new Date();
+    var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = 'Date : ' + date + ' Time : ' + time;
+    $(".responseText").text("Current student information has been loaded from DataBase");
+    $(".dateTime").text(dateTime);
+    $('#messageDisplay').modal('show');
+}
+
+function loadStudentData() {
+    var dataObject = {
+        dataType: "json",
+        action: 'read'
+    };
+    $.ajax({
+        data: dataObject,
+        dataType: "json",
+        method: "get",
+        url: 'php/actions/datapoint.php',
+        success: function (data) {
+            mainStudentArray = data.data;
+            showLoadModal();
+            updateStudentList(mainStudentArray);
+        },
+        error: function () {
+            console.log('error lodaing student data');
+            $(".responseText").text("Error- Unable to access current student information");
+            showLoadModal();
+        }
+    });
+    $.ajax(dataObject);
+}
+
+function renderStudentsOnDom(studentArray) {
+    $('#studentDisplay').empty();
+    for (var studentArrayIndex = 0; studentArrayIndex < studentArray.length; studentArrayIndex++) {
+        (function () {
+            var student = studentArray[studentArrayIndex];
+            var sName = $("<td>").text(student.name);
+            var sCourse = $("<td>").text(student.course);
+            var sGrade = $("<td>").text(student.grade);
+            var id = student.id;
+            var studentRow = $("<tr>").attr('id', id);
+            var toDelete = $("<button>", {
+                type: "button",
+                class: "btn btn-danger btn-xs",
+                id: "delete",
+                text: "Delete",
+                on: {
+                    click: (function (newRow) {
+                        return function () {
+                            studentToDelete(newRow);
+                        };
+                    })(studentRow)
+                }
+            });
+
+            function studentToDelete(parentRow) {
+                showDeleteModal(student);
+                $("#deleteConfirmBtn").click(function () {
+                    mainStudentArray.splice(student, 1);
+                    deleteStudent(student, parentRow);
+                })
+            }
+            var deleteBtn = $("<td>").append(toDelete)
+            studentRow.append(sName, sCourse, sGrade, deleteBtn);
+            $(".student-list").append(studentRow);
+        })();
+    }
+    calculateGradeAverage(studentArray);
+}
+
+function addStudent(studentName, studentCourse, studentGrade) {
+    var studentObj = { name: studentName, course: studentCourse, grade: studentGrade };
+    mainStudentArray.push(studentObj);
+    clearAddStudentFormInputs();
+
+    var dataObject = {
+        dataType: "json",
+        name: studentName,
+        course: studentCourse,
+        grade: studentGrade,
+        action: 'create'
+    };
+    $.ajax({
+        data: dataObject,
+        dataType: "json",
+        method: "post",
+        url: 'php/actions/datapoint.php',
+        success: function (response) {
+            console.log("server response from add student", response);
+            newStudentId = (response.id + '');
+            mainStudentArray[mainStudentArray.length - 1].id = newStudentId;
+            updateStudentList();
+        },
+        error: function () {
+            console.log("error adding student");
+        }
+    });
+    $.ajax(dataObject);
+    calculateGradeAverage(mainStudentArray);
+}
+
+function updateStudentList() {
+    renderStudentsOnDom(mainStudentArray);
+}
+
+function showDeleteModal(student) {
+    $(".nameText").text("Name : " + student.name);
+    $(".courseText").text("Course : " + student.course);
+    $(".gradeText").text("Grade : " + student.grade);
+    $('#deleteStudentModal').modal('show');
+}
+
+function deleteStudent(studentToDelete, parentRow) {
+    updateListOnDelete(studentToDelete, parentRow);
+}
+
+function updateListOnDelete(studentToDelete, parentRow) {
+    var dataObject = {
+        dataType: "json",
+        student_id: studentToDelete.id,
+        action: 'delete'
+    };
+    $.ajax({
+        data: dataObject,
+        dataType: "json",
+        method: "post",
+        url: 'php/actions/datapoint.php',
+        success: function (response) {
+            console.log('delete response ', response);
+            calculateGradeAverage(mainStudentArray);
+            renderGradeAverage();
+            parentRow.remove();
+        },
+        error: function () {
+            console.log("delete response error");
+        }
+    });
+    $.ajax(dataObject);
+
+}
+
 function sortNameOrCourse() {
     var studentArray = mainStudentArray.slice();
     var sortMode = $(this).attr('id');
@@ -84,187 +263,6 @@ function sortGrades() {
             renderStudentsOnDom(studentArray);
             break;
     }
-}
-
-
-function handleAddClicked() {
-    var name = $("#studentName").val();
-    var course = $("#studentCourse").val();
-    var grade = $("#studentGrade").val();
-    if (typeof name !== 'string' || name.length < 2 || !isNaN(name)) {
-        $(".errorText").text('Name must be specified');
-        $("#errorDisplay").modal('show');
-        return;
-    }
-    if (typeof course !== 'string' || course.length < 2 || !isNaN(course)) {
-        $(".errorText").text('Course must be specified');
-        $("#errorDisplay").modal('show');
-        return;
-    }
-    if (isNaN(grade) || grade < 0 || grade > 100 || grade.length < 1) {
-        $(".errorText").text('Grade must be a number between 0 - 100');
-        $("#errorDisplay").modal('show');
-        return;
-    }
-
-    addStudent(name, course, grade);
-}
-
-function handleCancelClick() {
-    $("#studentName").val("");
-    $("#studentCourse").val("");
-    $("#studentGrade").val("");
-}
-
-function showLoadModal() {
-    var today = new Date();
-    var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = 'Date : ' + date + ' Time : ' + time;
-    $(".responseText").text("Current student information has been loaded from DataBase");
-    $(".dateTime").text(dateTime);
-    $('#messageDisplay').modal('show');
-}
-
-function loadStudentData() {
-    var dataObject = {
-        dataType: "json",
-        action: 'read'
-    };
-    $.ajax({
-        data: dataObject,
-        dataType: "json",
-        method: "get",
-        url: 'php/actions/datapoint.php',
-        success: function (data) {
-            mainStudentArray = data.data;
-            showLoadModal();
-            updateStudentList(mainStudentArray);
-        },
-        error: function () {
-            console.log('error lodaing student data');
-            $(".responseText").text("Error- Unable to access current student information");
-            showLoadModal();
-        }
-    });
-    $.ajax(dataObject);
-}
-
-function addStudent(studentName, studentCourse, studentGrade) {
-    var studentObj = { name: studentName, course: studentCourse, grade: studentGrade };
-    getDataFromServer.data.push(studentObj);
-    clearAddStudentFormInputs();
-
-    var dataObject = {
-        dataType: "json",
-        name: studentName,
-        course: studentCourse,
-        grade: studentGrade,
-        action: 'create'
-    };
-    $.ajax({
-        data: dataObject,
-        dataType: "json",
-        method: "post",
-        url: 'php/actions/datapoint.php',
-        success: function (response) {
-            console.log("server response from add student", response);
-            newStudentId = (response.id + '');
-            getDataFromServer.data[getDataFromServer.data.length - 1].id = newStudentId;
-            updateStudentList();
-        },
-        error: function () {
-            console.log("error adding student");
-        }
-    });
-    $.ajax(dataObject);
-    calculateGradeAverage(getDataFromServer);
-}
-
-function showDeleteModal(student) {
-    $(".nameText").text("Name : " + student.name);
-    $(".courseText").text("Course : " + student.course);
-    $(".gradeText").text("Grade : " + student.grade);
-    $('#deleteStudentModal').modal('show');
-}
-
-function deleteStudent(studentToDelete, parentRow) {
-    // parentRow.remove();
-    updateListOnDelete(studentToDelete, parentRow);
-}
-
-function updateListOnDelete(studentToDelete, parentRow) {
-    var dataObject = {
-        dataType: "json",
-        student_id: studentToDelete.id,
-        action: 'delete'
-    };
-    $.ajax({
-        data: dataObject,
-        dataType: "json",
-        method: "post",
-        url: 'php/actions/datapoint.php',
-        success: function (response) {
-            console.log('delete response ', response);
-            calculateGradeAverage(getDataFromServer);
-            renderGradeAverage();
-            parentRow.remove();
-        },
-        error: function () {
-            console.log("delete response error");
-        }
-    });
-    $.ajax(dataObject);
-
-}
-
-function updateStudentList() {
-    renderStudentsOnDom(mainStudentArray);
-}
-
-function clearAddStudentFormInputs() {
-    $("#studentName").val("");
-    $("#studentCourse").val("");
-    $("#studentGrade").val("");
-}
-
-function renderStudentsOnDom(studentArray) {
-    $('#studentDisplay').empty();
-    for (var studentArrayIndex = 0; studentArrayIndex < studentArray.length; studentArrayIndex++) {
-        (function () {
-            var student = studentArray[studentArrayIndex];
-            var sName = $("<td>").text(student.name);
-            var sCourse = $("<td>").text(student.course);
-            var sGrade = $("<td>").text(student.grade);
-            var id = student.id;
-            var studentRow = $("<tr>").attr('id', id);
-            var toDelete = $("<button>", {
-                type: "button",
-                class: "btn btn-danger btn-xs",
-                id: "delete",
-                text: "Delete",
-                on: {
-                    click: (function (newRow) {
-                        return function () {
-                            studentToDelete(newRow);
-                        };
-                    })(studentRow)
-                }
-            });
-
-            function studentToDelete(parentRow) {
-                showDeleteModal(student);
-                $("#deleteConfirmBtn").click(function () {
-                    getDataFromServer.data.splice(student, 1);
-                    deleteStudent(student, parentRow);
-                })
-            }
-            var deleteBtn = $("<td>").append(toDelete)
-            studentRow.append(sName, sCourse, sGrade, deleteBtn);
-            $(".student-list").append(studentRow);
-        })();
-    }
-    calculateGradeAverage(studentArray);
 }
 
 function calculateGradeAverage(array) {
